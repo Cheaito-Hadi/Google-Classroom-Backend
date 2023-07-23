2961742d8ef84a8acfb1de84aa7373384188791f
unknown <youssefhoussein701@gmail.com>
2023-07-23 14:38:06 +0300

    modify the teachers and students scripts

--- students.php
+++ students.php
@@ -4,7 +4,7 @@ include('connection.php');
 
 $class_room_id =1;
 
-$query = $sql->prepare('select user_id from students where id_classRoom = ?');
+$query = $sql->prepare('select first_name, last_name from users u join students s on u.id = s.user_id where s.id_classRoom = ?');
 $query->bind_param("i",$class_room_id);
 $query->execute();
 $array = $query->get_result();
@@ -13,16 +13,10 @@ $response =[];
 while($student = $array->fetch_assoc()){
     $response[] = $student;
 }
-foreach($response as $student){
-    $id = $student['user_id'];
-    $student_info = $sql->prepare('select first_name, last_name from users where id = ?');
-    $student_info->bind_param("i",$id);
-    $student_info->execute();
-    $array_of_students =  $student_info->get_result();
-    $student_response =[];
-    while($student_result = $array_of_students->fetch_assoc()){
-        $student_response[] = $student_result;
-    }
-    echo json_encode($student_response);
-}
+echo json_encode($response);
+
+
+
+
+
 
--- teachers.php
+++ teachers.php
@@ -2,10 +2,10 @@
 
 include('connection.php');
 
-$user_id =1;
+$class_room_id =1;
 
-$query = $sql->prepare('select teacher_id from teachers where classRoom_id_teacher = ?');
-$query->bind_param("i",$user_id);
+$query = $sql->prepare('select first_name, last_name from users u join teachers t on u.id = t.teacher_id where t.classRoom_id_teacher = ?');
+$query->bind_param("i",$class_room_id);
 $query->execute();
 $array = $query->get_result();
 $response =[];
@@ -13,17 +13,6 @@ $response =[];
 while($teacher = $array->fetch_assoc()){
     $response[] = $teacher;
 }
-foreach($response as $teacher){
-    $id = $teacher['teacher_id'];
-    $teacher_info = $sql->prepare('select first_name, last_name from users where id = ?');
-    $teacher_info->bind_param("i",$id);
-    $teacher_info->execute();
-    $array_of_teachers =  $teacher_info->get_result();
-    $teacher_response =[];
-    while($teacher_result = $array_of_teachers->fetch_assoc()){
-        $teacher_response[] = $teacher_result;
-    }
-    echo json_encode($teacher_response);
-}
+echo json_encode($response);
 
 
