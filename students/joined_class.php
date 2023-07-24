1bc22eb48efbabc12b8870c488ac59cc4253f89c
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-24 12:29:09 +0300

    get all student info feom db

--- students/joined_class.php
+++ students/joined_class.php
@@ -16,12 +16,33 @@ if (isset($_POST['user_id'])) {
         $query = $mysqli->prepare('INSERT INTO Students (id_classRoom, user_id) VALUES (?, ?)');
         $query->bind_param('ss', $class_id, $user_id);
         $query->execute();
+
+        $get_student = $mysqli->prepare('SELECT * FROM Students WHERE user_id = ?');
+        $get_student->bind_param('s', $user_id);
+        $get_student->execute();
+        $get_student->store_result();
+        $get_student->bind_result($student_id, $user_id, $classRoom_id);
+    
+        $student_array = array();
+        
+        while ($get_student->fetch()) {
+            $student_row = array(
+                'student_id' => $student_id,
+                'user_id' => $user_id,
+                'classRoom_id' => $classRoom_id,
+            );
+            
+            $student_array[] = $student_row;
+        }
+
         $response['status'] = 'success';
         $response['message'] = 'you just joined the class';
+        $response['student'] = $student_array;
     } else {
         $response['status'] = 'failed';
         $response['message'] = 'already joined the class';
     }
+
 } else {
     $response['status'] = 'failed';
     $response['message'] = 'no user found';
