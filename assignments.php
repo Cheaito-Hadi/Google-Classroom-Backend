29a36954c6109037cba50296436629ddad5b14db
unknown <youssefhoussein701@gmail.com>
2023-07-24 14:18:08 +0300

    add the script to retrieve the data of asssignment

---
+++ assignments.php
@@ -0,0 +1,19 @@
+<?php
+
+include('connection.php');
+
+$classroom_id =2;
+
+$query = $sql->prepare('SELECT a.title, a.instarction, a.due_date, u.first_name, u.last_name FROM assignmnt a JOIN teachers t ON a.teachers_id = t.id JOIN users u ON t.teacher_id = u.id WHERE a.id = ?');
+$query->bind_param("i",$classroom_id);
+
+$query->execute();
+$array = $query->get_result();
+$response =[];
+
+while($assignment = $array->fetch_assoc()){
+    $response[] = $assignment;
+}
+echo json_encode($response);
+
+
