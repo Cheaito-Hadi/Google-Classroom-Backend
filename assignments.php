e2b2ac56f9f7a774421cb627442ebbef3c74ce01
unknown <youssefhoussein701@gmail.com>
2023-07-24 15:17:14 +0300

    fix the date format

--- assignments.php
+++ assignments.php
@@ -4,7 +4,7 @@ include('connection.php');
 
 $classroom_id =2;
 
-$query = $sql->prepare('SELECT a.title, a.instarction, a.due_date, a.post_date, u.first_name, u.last_name FROM assignmnt a JOIN teachers t ON a.teachers_id = t.id JOIN users u ON t.teacher_id = u.id WHERE a.id = ?');
+$query = $sql->prepare('SELECT a.title, a.instarction, DATE_FORMAT(a.due_date, "%b %e %h:%i %p") as due_date, DATE_FORMAT(a.post_date, "%b %e") as post_date, u.first_name, u.last_name FROM assignmnt a JOIN teachers t ON a.teachers_id = t.id JOIN users u ON t.teacher_id = u.id WHERE a.id = ?');
 $query->bind_param("i",$classroom_id);
 
 $query->execute();
