6aa6c1d6bbb356dabade32beb24affcf60ea34e2
unknown <youssefhoussein701@gmail.com>
2023-07-25 20:39:23 +0300

    add the turned in

--- assignment_uploads.php
+++ assignment_uploads.php
@@ -1,11 +1,6 @@
 <?php
 include('connection.php');
 
-
-
-// $fileData = file_get_contents($_FILES["fileToUpload"]["tmp_name"]);
-// $student_id = $_POST["student_id"];
-// $assignment_id = $_POST["assignment_id"];
 if (isset($_POST['student_id']) && isset($_POST['assignment_id']) && isset($_FILES['fileToUpload'])) {
     $student_id = $_POST['student_id'];
     $assignment_id = $_POST['assignment_id'];
@@ -26,31 +21,5 @@ if (isset($_POST['student_id']) && isset($_POST['assignment_id']) && isset($_FIL
     echo json_encode(array("error" => "Missing form data."));
 }
 
-
-
-
-// if (isset($_POST['student_id']) && isset($_POST['assignment_id']) && isset($_FILES['fileToUpload'])) {
-//     $student_id = $_POST['student_id'];
-//     $assignment_id = $_POST['assignment_id'];
-    
-//     // Check if the file was uploaded successfully
- 
-//         // Insert the data into the database as BLOB
-//         $stmt = $conn->prepare("INSERT INTO assignment_uploads (student_id, assignment_id, file) VALUES (?, ?, ?)");
-//         $stmt->bind_param("sss", $student_id, $assignment_id, $fileData);
-
-//         if ($stmt->execute()) {
-//             echo json_encode(array("message" => "File uploaded and data inserted successfully."));
-//         } else {
-//             echo json_encode(array("error" => "Error: " . $stmt->error));
-//         }
-
-//         $stmt->close();
-//     } else {
-//         echo json_encode(array("error" => "File upload error: " . $_FILES['fileToUpload']['error']));
-//     }
-// } else {
-//     echo json_encode(array("error" => "Missing form data."));
-// }
 
 
---
+++ turn_assignment_in.php
@@ -0,0 +1,15 @@
+<?php
+
+include('connection.php');
+
+$student_id = $_POST['student_id'];
+$assignment_id = $_POST['assignment_id'];
+
+$query = $sql->prepare('INSERT INTO turned_in ( `student_id`, `assignment_id`) VALUES (?,?)');
+$query->bind_param("ii",$student_id,$assignment_id);
+
+if ($query->execute()) {
+    echo json_encode(array("message" => "Turned in"));
+   } else {
+    echo json_encode(array("error" => "Error: " . $q->error));
+}
