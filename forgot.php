5c80ca5daa2005bd928a8ea1384bee8b46dd43ac
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-07-26 06:40:53 +0300

    forget passowrd, google link api

--- forgot.php
+++ forgot.php
@@ -12,22 +12,9 @@ $selectResult = $query->get_result();
 $userData = $selectResult->fetch_assoc();
 
 if ($selectResult->num_rows > 0) {
-    $new_password = $_POST["new_password"];
-
-    $updateQuery = $mysqli->prepare("UPDATE users SET password = ? WHERE email = ? AND questions = ?");
-    $updateQuery->bind_param("sss", $new_password, $recovery_email, $recovery_answer);
-    $updateResult = $updateQuery->execute();
-
-    if ($updateResult) {
-        $response["status"] = "success";
-        $response["message"] = "Password updated successfully!";
-    } else {
-        $response["status"] = "error";
-        $response["message"] = "Failed to update password.";
-    }
+    $response["status"] = "success";
 } else {
     $response["status"] = "error";
-    $response["message"] = "User not found with the provided email and security answer.";
 }
 
 echo json_encode($response);
--- share_google_meet.php
+++ share_google_meet.php
@@ -3,23 +3,11 @@
 include('connection.php');
 
 $google_link = $_POST['google_link'];
-$classroom_name = $_POST['name'];
-$check_name = $mysqli->prepare('SELECT name FROM classes_room WHERE name=?');
-$check_name->bind_param('s', $classroom_name);
-$check_name->execute();
-$check_name->store_result();
-$name_exists = $check_name->num_rows();
+$classroom_id = $_POST['classroom_id'];
 
-if ($name_exists > 0) {
-    $query = $mysqli->prepare('UPDATE classes_room SET google_link=? WHERE name=?');
-    $query->bind_param('ss', $google_link, $classroom_name);
-    $query->execute();
-} else {
-    $response['status'] = "failed";
-    echo json_encode($response);
-}
+$query = $mysqli->prepare('UPDATE classes_room SET google_link=? WHERE id=?');
+$query->bind_param('ss', $google_link, $classroom_id);
+$query->execute();
 
 $response['status'] = "success";
 echo json_encode($response);
-?>
-
